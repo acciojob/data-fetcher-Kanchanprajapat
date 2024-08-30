a644d1c698047c112e96c102e4d39a079bfef4a6
kanchan <kp4307699@gmail.com>
2024-08-30 20:25:03 +0530

    pushed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import 'regenerator-runtime/runtime';
 
-const App = () => {
+const DataFetcher = () => {
+  const [apiData, setApiData] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        
+        const data = await response.json();
+        
+        if (data) {
+          setApiData(data);
+        } else {
+          setApiData([]);
+        }
+      } catch (error) {
+        setError('An error occurred: ' + error.message);
+        setApiData([]);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+  if(apiData===undefined){
+   return <h1>No data found</h1>
+  }
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      {apiData?  (
+        <>
+          <h1 id="header">Data Fetched from API</h1>
+          <pre id="output">{JSON.stringify(apiData, null, 2)|| apiData}</pre>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default DataFetcher;
